d36a393d66f5edc6693e67bfabef405679280cc0
ushi <>
2025-11-15 18:26:50 +0900

    feature: async todo notifier

---
+++ lib/domain/logic/async_todo_notifier.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_stream_todo_app/data/repository/todo_repository.dart';
+import 'package:flutter_riverpod_stream_todo_app/domain/model/todo.dart';
+
+// Provider定義
+final asyncTodoNotifierProvider =
+    AsyncNotifierProvider<TodoNotifier, List<Todo>>(() => TodoNotifier());
+
+class TodoNotifier extends AsyncNotifier<List<Todo>> {
+  @override
+  Future<List<Todo>> build() async {
+    final streamTodos = ref.watch(todoRepositoryProvider).fetchTodos();
+    final todos = await streamTodos.first;
+    return todos.map((todoDto) => Todo(title: todoDto.title)).toList();
+  }
+
+  // Todoを追加するメソッド
+  Future<void> addTodo(String title) async {
+    await ref.read(todoRepositoryProvider).addTodo(title);
+  }
+}
