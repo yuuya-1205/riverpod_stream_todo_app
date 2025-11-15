90974212ee60b4ce8591fb03f60293361ca44bbe
ushi <>
2025-11-15 11:52:45 +0900

    feature: ProviderScope

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_stream_todo_app/firebase_options.dart';
+import 'package:flutter_riverpod_stream_todo_app/presentation/view/todo_list_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(ProviderScope(observers: [], child: const MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const TodoListPage(),
+    );
+  }
+}
