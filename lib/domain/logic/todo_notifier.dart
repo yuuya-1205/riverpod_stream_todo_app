e6a015cf86f90ecec8d74463f4f6e1399be58023
ushi <>
2025-11-15 18:01:20 +0900

    feature: TodoDto to todo

--- lib/domain/logic/todo_notifier.dart
+++ lib/domain/logic/todo_notifier.dart
@@ -1,30 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod_stream_todo_app/data/dto/todo_dto.dart';
-import 'package:flutter_riverpod_stream_todo_app/data/repository/firebase_firestore_repository.dart';
 import 'package:flutter_riverpod_stream_todo_app/data/repository/todo_repository.dart';
+import 'package:flutter_riverpod_stream_todo_app/domain/model/todo.dart';
 
 // Provider定義
-final todoNotifierProvider =
-    StreamNotifierProvider<TodoNotifier, List<TodoDto>>(() => TodoNotifier());
+final todoNotifierProvider = StreamNotifierProvider<TodoNotifier, List<Todo>>(
+  () => TodoNotifier(),
+);
 
-class TodoNotifier extends StreamNotifier<List<TodoDto>> {
+class TodoNotifier extends StreamNotifier<List<Todo>> {
   @override
-  Stream<List<TodoDto>> build() {
-    final firebaseFirestore = ref.read(firebaseFirestoreProvider);
-    return firebaseFirestore
-        .collection('todos')
-        .snapshots()
-        .map(
-          (snapshot) => snapshot.docs
-              .map((doc) => TodoDto(title: doc.data()['title'] as String))
-              .toList(),
-        );
+  Stream<List<Todo>> build() {
+    final stream = ref.watch(todoRepositoryProvider).fetchTodos();
+    return stream.map(
+      (todos) => todos.map((todo) => Todo(title: todo.title)).toList(),
+    );
   }
 
   // Todoを追加するメソッド
   Future<void> addTodo(String title) async {
-    final repository = ref.read(todoRepositoryProvider);
-    await repository.addTodo(title);
-    // ストリームが自動的に更新されるので、特別な処理は不要
+    await ref.read(todoRepositoryProvider).addTodo(title);
   }
 }
