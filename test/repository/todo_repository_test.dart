8d8bb6c8e6f2ef454d98b95a211cef9c2ce3693a
ushi <>
2025-11-21 07:47:29 +0900

    fix: todo repository test

--- test/repository/todo_repository_test.dart
+++ test/repository/todo_repository_test.dart
@@ -11,7 +11,12 @@ void main() {
   setUp(() {
     mockTodoRepository = MockTodoRepository();
   });
-  test('addTodo', () async {
+  test('fetchTodos', () async {
+    // 前提 : Firebaseから取得した値がTodoDto(title: 'test') を確認するテスト。
+    // 期待値 : [TodoDto(title: 'test')] が取得できていることが確認できる。。
+
+    // モックを設定する。
+    // ここは今回のテストの答え（こうなるよ！）ってことを記載する。
     when(
       mockTodoRepository.fetchTodos(),
     ).thenAnswer((_) => Stream.value([TodoDto(title: 'test')]));
@@ -24,6 +29,29 @@ void main() {
     );
     final todoRepository = container.read(todoRepositoryProvider);
     final todos = await todoRepository.fetchTodos().first;
+    expect(todos, [TodoDto(title: 'test')]);
+  });
+
+  test('addTodo', () async {
+    // 前提 : Todoを追加するメソッドが呼ばれたことを確認するテスト。
+    // 期待値 : Todoが追加されたことが確認できる。。
+
+    // モックを設定する。
+    // Streamで初期は空のリストを返すようにする。（前提は何もない状態）
+    // ここは今回のテストの正解を記載するところ。
+    // 今回の正解はadd関数を使ってTodoDto(title: 'test')を追加した時に、
+    //　　fetchTodosでTodoDto(title: 'test')が取得できるようにする。
+    when(
+      mockTodoRepository.fetchTodos(),
+    ).thenAnswer((_) => Stream.value([TodoDto(title: 'test')]));
+
+    // add関数を用いてTodoDto(title: 'test')を追加する。
+    await mockTodoRepository.addTodo('test');
+
+    // ここでfetchTodosを呼び出して、TodoDto(title: 'test')が追加されていることを確認する。
+    final todos = await mockTodoRepository.fetchTodos().first;
+
+    // ここでfetchTodosでTodoDto(title: 'test')が取得できることを確認する。
     expect(todos, [TodoDto(title: 'test')]);
   });
 }
